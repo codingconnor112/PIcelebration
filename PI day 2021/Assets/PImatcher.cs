9fb686f5ce06c61651710ed5d991c0c0fed3090d
codingconnor112 <nguyenfamily811810@gmail.com>
2021-03-17 20:05:14 -0700

    added game

---
+++ PI day 2021/Assets/PImatcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PImatcher : MonoBehaviour
+{
+    public decimal realpi = 3.14159265358979323m;
+ 
+    public Slider slider;
+    public Text text;
+    decimal pi(int depth)
+    {
+        return System.Math.Round(realpi, depth);
+    }
+    private void Start()
+    {
+
+        int count = BitConverter.GetBytes(decimal.GetBits(realpi)[3])[2];
+        slider.maxValue = count;
+        slider.onValueChanged.AddListener(delegate { set(); });
+    }
+    void set()
+    {
+        string texter = pi((int)slider.value).ToString();
+        text.text = "PI="+texter;
+    }
+}
---
+++ PI day 2021/Assets/digitdisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class digitdisplay : MonoBehaviour
+{
+    public Text text;
+    public Slider slider;
+    private void Start()
+    {
+        slider.onValueChanged.AddListener(delegate { set(); });
+    }
+    void set()
+    {
+        string val = slider.value.ToString();
+        text.text = val;
+    }
+}
